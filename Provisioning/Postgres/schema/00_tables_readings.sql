3ce4cec25fa4f2870eaad6a09352fd9b591eea4f
Greg <gswiatek@gmail.com>
2026-05-07 08:37:16 -0700

    update

--- Provisioning/Postgres/schema/00_tables_readings.sql
+++ Provisioning/Postgres/schema/00_tables_readings.sql
@@ -15,6 +15,22 @@ CREATE TABLE IF NOT EXISTS v1.sensor_readings (
 -- Convert to hypertable
 SELECT create_hypertable('v1.sensor_readings', 'timestamp_utc', if_not_exists => TRUE);
 
+-- Indexes for sensor_readings
+CREATE INDEX IF NOT EXISTS sensor_readings_timestamp_utc_idx 
+    ON v1.sensor_readings (timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_sensor_readings_type 
+    ON v1.sensor_readings (type);
+    
+CREATE INDEX IF NOT EXISTS idx_sensor_readings_sensor_time 
+    ON v1.sensor_readings (sensor_id, timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_sensor_readings_device_time 
+    ON v1.sensor_readings (device_id, timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_sensor_readings_account_time 
+    ON v1.sensor_readings (account_id, timestamp_utc DESC);
+
 
 -- gateway_readings hypertable
 CREATE TABLE IF NOT EXISTS v1.gateway_readings (
@@ -30,3 +46,19 @@ CREATE TABLE IF NOT EXISTS v1.gateway_readings (
 
 -- Convert to hypertable
 SELECT create_hypertable('v1.gateway_readings', 'timestamp_utc', if_not_exists => TRUE);
+
+-- Indexes for gateway_readings
+CREATE INDEX IF NOT EXISTS gateway_readings_timestamp_utc_idx 
+    ON v1.gateway_readings (timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_gateway_readings_type 
+    ON v1.gateway_readings (type);
+    
+CREATE INDEX IF NOT EXISTS idx_gateway_readings_account_time 
+    ON v1.gateway_readings (account_id, timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_gateway_readings_device_time 
+    ON v1.gateway_readings (device_id, timestamp_utc DESC);
+    
+CREATE INDEX IF NOT EXISTS idx_gateway_readings_gateway_time 
+    ON v1.gateway_readings (gateway_id, timestamp_utc DESC);
